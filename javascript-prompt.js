57d2da60709167f1d83f59a9f7e4b11cef2e1e8d
Ach Muqoddam <ach.adam04@gmail.com>
2024-04-21 13:09:43 +0700

    membuat prompt untuk mengetahui saldo akhir yang di inputkan oleh user

---
+++ javascript-prompt.js
@@ -0,0 +1,7 @@
+// Membuat Varible dan Menggunakan prompt
+let saldoAwal = prompt('berapa saldo anda ?')
+let saldoKeluar = prompt('berapa saldo yang dikeluarkan ?')
+const saldoAkhir = saldoAwal - saldoKeluar
+
+// Menggunakan alert
+alert(`Sisa saldo anda Adalah Rp.${saldoKeluar}`)
